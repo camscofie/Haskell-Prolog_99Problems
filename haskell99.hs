8d054c9a1029e108b41b66b0d0ca0b0845374072
Yange Zheng <posterscofield@gmail.com>
2021-08-29 23:08:18 +0200

    haskell 1.03

--- haskell99.hs
+++ haskell99.hs
@@ -35,7 +35,29 @@ myButLast [a] = error "also not working"
 myButLast [a,_] = a
 myButLast (_:xs) = myButLast xs
 
+{-
+    Problem 3
+(*) Find the K'th element of a list. The first element in the list is number 1.
+
+Example:
+    * (element-at '(a b c d e) 3)
+    c
+
+Example in Haskell:
+    λ> elementAt [1,2,3] 2
+    2
+    λ> elementAt "haskell" 5
+    'e'
+-}
+
+elementAt :: (Ord t, Num t) => [p] -> t -> p
+elementAt [] _ = error "Not good"
+elementAt (x:xs) n
+    | n < 0 = error "Not good"
+    | n == 0 = x 
+    | otherwise = elementAt xs (n-1)
+
+
 -- test with main--
 main = do
-    print $ myButLast [4,5,6]
-    print $ myButLast ['a','b','c']
+    print $ elementAt [3,4,5] 3
