2f4e3f1ffc111c2d6f25f5a3172e3da71f013f3a
Yange Zheng <posterscofield@gmail.com>
2021-09-03 01:35:51 +0200

    1.10

--- haskell99.hs
+++ haskell99.hs
@@ -206,7 +206,41 @@ pack (x:xs)
     where 
         (y:ys) = pack xs
 
+{-
+Problem 10
+(*) Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+
+Example:
+
+* (encode '(a a a a b c c a a d e e e e))
+((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
+Example in Haskell:
+
+λ> encode "aaaabccaadeeee"
+[(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+-}
+
+countDupLengthMul :: [[a]] -> [(Int,a)]
+countDupLengthMul [] = []
+countDupLengthMul (x:xs) = countDupLength x : countDupLengthMul xs
+
+
+countDupLength :: [a] -> (Int, a)
+countDupLength (x:xs) = countDupLengthAcc (1, x) xs
+
+countDupLengthAcc :: (Int, a) -> [a] -> (Int, a)
+countDupLengthAcc (a, b) [] = (a, b)
+countDupLengthAcc (a, b) (x:xs) = countDupLengthAcc (a+1, b) xs
+
+
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode listIn = countDupLengthMul packedList
+    where 
+    packedList = pack listIn
+
 
 
 main = do
-    print $ pack "Haaallooo!!/"
+    print $ pack "Haaallooo!!///iiijiji"
+    print $ encode "Haaallooo!!/iiijiji"
